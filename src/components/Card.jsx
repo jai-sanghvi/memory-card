1e6940ebe21dd5db1c9aab894e383aae75abff58
Jai Sanghvi <85071341+jai-sanghvi@users.noreply.github.com>
2025-03-06 12:58:02 +0000

    Add default value to `Card`'s `name` prop
    Without a default value `name.toUpperCase()` throws an error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import '../styles/Card.css'
 
-export default function Card({clickHandler, name, img}) {
+export default function Card({clickHandler, name = '', img}) {
   return (
     <div className='card' onClick={clickHandler}>
       <img src={img} alt={name} />
